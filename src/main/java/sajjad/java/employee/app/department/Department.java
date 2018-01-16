fd48c45650599ebe285b40ba2d09c8111eca5500
Sajjad Hossain <my.lost.word@gmail.com>
2018-01-16 18:22:05 +0600

    Add department CRUD REST API

---
+++ src/main/java/sajjad/java/employee/app/department/Department.java
@@ -0,0 +1,43 @@
+package sajjad.java.employee.app.department;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name="department")
+public class Department {
+	
+	@Id
+	@Column(unique = true)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@NotNull
+	@NotBlank
+	@Column(nullable = false)
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
